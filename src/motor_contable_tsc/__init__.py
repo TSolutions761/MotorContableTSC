2bd5b8300fe62f2c42ae52c437bfa27bbc7508ac
TSolutions761 <jorge@ts-chile.com>
2026-07-14 15:36:29 -0400

    feat: exponer normalizador y exportador

--- src/motor_contable_tsc/__init__.py
+++ src/motor_contable_tsc/__init__.py
@@ -1,11 +1,25 @@
 """Motor Contable TSC."""
 
-from .cartola import leer_cartola, agrupar_movimientos
-from .models import MovimientoBancario, GrupoContable
+from .cartola import agrupar_movimientos, leer_cartola
+from .models import GrupoContable, MovimientoBancario
+from .normalizador import (
+    MovimientoNormalizado,
+    calcular_dv,
+    extraer_rut,
+    normalizar_movimiento,
+    normalizar_rut,
+)
+from .reporte import exportar_normalizacion_excel
 
 __all__ = [
     "MovimientoBancario",
     "GrupoContable",
+    "MovimientoNormalizado",
     "leer_cartola",
     "agrupar_movimientos",
+    "calcular_dv",
+    "normalizar_rut",
+    "extraer_rut",
+    "normalizar_movimiento",
+    "exportar_normalizacion_excel",
 ]
